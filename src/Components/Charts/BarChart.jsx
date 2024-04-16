0743ea57b6ae430040144f83b7cbcd407098abf5
Sandeep <sandeeps@bigit.io>
2024-04-16 16:21:50 +0530

    Changes in Total Profit chart

--- src/Components/Charts/BarChart.jsx
+++ src/Components/Charts/BarChart.jsx
@@ -1,34 +1,30 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 
-
-
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const dData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const tData = [2000, 1458, 6000, 9000, 5670, 8790, 2340];
+const tData = [6400, 9398, 9800, 5908, 1800, 8800, 4300];
+const tlData = [2000, 1198, 9000, 1908, 3800, 2700, 3400];
 
 const xLabels = [
   'Item One',
   'Item Two',
   'Item Three',
   'Item Four',
-//   'Page E',
-//   'Page F',
-//   'Page G',
+  'Item Five',
 ];
-
 
 export default function SimpleBarChart() {
   return (
     <BarChart
       height={300}
       series={[
-        { data: pData, label: 'Deposit Profit', id: 'pvId' },
-        { data: uData, label: 'Payout Profit', id: 'uvId' },
-        { data: tData, label: 'Transfer Profit', id: 'tvId' },
+        { data: dData, label: 'Deposit Profit', id: 'deposit' },
+        { data: tData, label: 'Transfer Profit', id: 'transfer' },
+        { data: pData, label: 'Payout Profit', id: 'payout' },
+        { data: tlData, label: 'Total Profit', id: 'total' },
       ]}
       xAxis={[{ data: xLabels, scaleType: 'band' }]}
-    //   colors={['orange', 'green', 'blue']}
     />
   );
 }
